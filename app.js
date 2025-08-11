4711c80024bc09302e78d44207d35940189b0810
Diego09DG <neumann.diego@escola.pr.gov.br>
2025-08-11 10:59:04 -0300

    index

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ function criaCartao(categoria, pergunta, resposta) {
         let cartao = document.createElement('article')
         cartao.className = 'cartao'
 
-cartao.innerHTML = `
+
+        cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
@@ -14,9 +15,17 @@ cartao.innerHTML = `
     </div>
     </div>
     `
-    let respostaEstaVisivel = false
+
+
+let respostaEstaVisivel = false
 
     function viraCartao() {
     respostaEstaVisivel = !respostaEstaVisivel
     cartao.classList.toggle('active', respostaEstaVisivel)
 }
+
+cartao.addEventListener('click', viraCartao)
+container.appendChild(cartao)
+}
+
+
